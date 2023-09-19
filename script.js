660a89f6907adffd19101436ade2125ad1b8e75b
Eledah <eledah.niles@yahoo.com>
2023-09-19 09:59:25 +0330

    chart.js, css, table of content

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-const fileNames = [
-    'items/آویز گردنبند طلا 18 عیار زنانه.json',
-    'items/آیفون ۱۳ نان‌اکتیو.json',
-    'items/آینه بغل پراید.json',
-    'items/برنج هاشمی ممتاز طبیعت - 10 کیلوگرم.json',
-    'items/تلویزیون ال ای دی هوشمند ایکس ویژن.json',
-    'items/تونر پاک کننده صورت لافارر.json',
-    'items/خیار شور سالی - 1.5 کیلوگرم.json',
-    'items/رژ لب جامد الیزا مدل Mini.json',
-    'items/زیرپوش رکابی مردانه امیدنو.json',
-    'items/عسل طبیعی سبلان.json',
-    'items/فلش مموری کوئین تک.json',
-    'items/ماشین لباسشویی دوو.json',
-    'items/موتور برق آسترا.json',
-    'items/هندزفری تسکو مدل TH 5052.json',
-    'items/پنیر سفید فتا کاله.json',
-    'items/پوشک بچه هانیز.json',
-    'items/کتاب هر دو در نهایت می میرند.json',
-    'items/کنسول Playstation 5.json',
-    'items/کیف کمری چرمی.json',
-    'items/گوشی موبایل شیائومی مدل Redmi Note 12 4G.json',
-    'items/sample.json'
-];
-
-// Function to fetch and draw scatter plot from a local JSON file
+// Function to fetch and draw scatter plot from a local JSON file using Chart.js
 function drawScatterPlot(filename) {
     // Extract the chart name from the filename (remove ".json" at the end and "items/")
     const chartName = filename.replace('items/', '').replace('.json', '');
@@ -37,57 +13,151 @@ function drawScatterPlot(filename) {
             // Format dates to display only Day, Month, and Year
             const formattedDates = dates.map(date => new Date(date).toISOString().slice(0, 10));
 
-            const trace = {
-                x: formattedDates,
-                y: prices,
-                mode: 'markers',
-                type: 'scatter',
-                marker: { 
-                    size: 8,
-                    color: discounts.map(discount => discount > 0 ? 'red' : 'green') // Color points based on discount
+            const datasets = [{
+                label: chartName,
+                data: prices,
+                borderColor: discounts.map(discount => discount > 0 ? 'red' : 'green'), // Color points based on discount
+                backgroundColor: 'transparent', // Transparent background
+                pointRadius: 6, // Point size
+                pointBackgroundColor: discounts.map(discount => discount > 0 ? 'red' : 'green'), // Color points based on discount
+                pointBorderColor: 'transparent', // Transparent point border
+                pointBorderWidth: 0, // Point border width
+            }];
+
+            const chartData = {
+                labels: formattedDates,
+                datasets: datasets,
+            };
+
+            const chartOptions = {
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'day', // Display only Day, Month, and Year
+                        },
+                    },
+                    y: {
+                        beginAtZero: true,
+                        max: Math.ceil(Math.max(...prices)), // Set max Y-axis value
+                        callback: function (value) {
+                            return value.toFixed(1); // Round Y-axis labels to 1 decimal place
+                        },
+                    },
                 },
-                line: {
-                    color: discounts.map(discount => discount > 0 ? 'lightred' : 'lightgreen'), // Color lines based on discount
-                    width: 1 // Line width
-                }
             };
 
-            const layout = {
-                title: '', // Empty title (no need for a title as <h3> contains the chart name)
-                xaxis: { title: 'Date' },
-                yaxis: { title: 'Price' }
-            };
-
-            const plotData = [trace];
-
-            // Create a new div for each plot
-            const div = document.createElement('div');
-            div.className = 'scatter-plot';
-            document.getElementById('scatter-plots').appendChild(div);
-
-            // Create an <h3> tag for the chart name and set its text content
-            const chartTitle = document.createElement('h3');
-            chartTitle.textContent = chartName; // Set the chart name
-            chartTitle.style.color = 'darkblue'; // Apply dark blue color
-
-            // Create a container <div> for <h3> and the scatter plot
             const chartContainer = document.createElement('div');
-            chartContainer.className = 'chart-container';
-
-            // Append the <h3> and the scatter plot to the container <div>
-            chartContainer.appendChild(chartTitle);
-            chartContainer.appendChild(div);
-
-            // Append the container <div> to the scatter-plots container
+            chartContainer.className = 'scatter-plot';
             document.getElementById('scatter-plots').appendChild(chartContainer);
 
-            // Render the scatter chart in the new div
-            Plotly.newPlot(div, plotData, layout);
+            // Create a <h3> tag for the chart name and set its text content
+            const chartTitle = document.createElement('h3');
+            chartTitle.textContent = chartName;
+            chartTitle.style.color = 'darkblue';
+
+            // Append the <h3> and the chart canvas to the chart container
+            chartContainer.appendChild(chartTitle);
+
+            // Create a canvas element for the Chart.js chart
+            const canvas = document.createElement('canvas');
+            chartContainer.appendChild(canvas);
+
+            // Create the Chart.js scatter plot
+            new Chart(canvas, {
+                type: 'scatter',
+                data: chartData,
+                options: chartOptions,
+            });
         })
         .catch(error => console.error('Error loading data:', error));
 }
 
-// Loop through the file names and draw scatter plots
-for (const fileName of fileNames) {
-    drawScatterPlot(fileName);
+// Array of local JSON file names and corresponding chart ids
+const chartData = [
+    {
+        fileName: 'items/آویز گردنبند طلا 18 عیار زنانه.json',
+        chartId: 'chart-1' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/آیفون ۱۳ نان‌اکتیو.json',
+        chartId: 'chart-2' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/آینه بغل پراید.json',
+        chartId: 'chart-3' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/برنج هاشمی ممتاز طبیعت - 10 کیلوگرم.json',
+        chartId: 'chart-4' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/تلویزیون ال ای دی هوشمند ایکس ویژن.json',
+        chartId: 'chart-5' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/تونر پاک کننده صورت لافارر.json',
+        chartId: 'chart-6' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/خیار شور سالی - 1.5 کیلوگرم.json',
+        chartId: 'chart-7' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/رژ لب جامد الیزا مدل Mini.json',
+        chartId: 'chart-8' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/زیرپوش رکابی مردانه امیدنو.json',
+        chartId: 'chart-9' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/عسل طبیعی سبلان.json',
+        chartId: 'chart-10' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/فلش مموری کوئین تک.json',
+        chartId: 'chart-11' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/ماشین لباسشویی دوو.json',
+        chartId: 'chart-12' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/موتور برق آسترا.json',
+        chartId: 'chart-13' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/هندزفری تسکو مدل TH 5052.json',
+        chartId: 'chart-14' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/پنیر سفید فتا کاله.json',
+        chartId: 'chart-15' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/پوشک بچه هانیز.json',
+        chartId: 'chart-16' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/کتاب هر دو در نهایت می میرند.json',
+        chartId: 'chart-17' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/کنسول Playstation 5.json',
+        chartId: 'chart-18' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/کیف کمری چرمی.json',
+        chartId: 'chart-19' // Unique id for the chart container
+    },
+    {
+        fileName: 'items/گوشی موبایل شیائومی مدل Redmi Note 12 4G.json',
+        chartId: 'chart-20' // Unique id for the chart container
+    },
+];
+
+// Loop through the chart data and draw scatter plots using Chart.js
+for (const data of chartData) {
+    drawScatterPlot(data.fileName, data.chartId);
 }
