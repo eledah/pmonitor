85f207aa70dd0ec28ab7de073180e72376b5f030
Eledah <eledah.niles@yahoo.com>
2023-09-19 10:44:59 +0330

    rtl, toc

--- script.js
+++ script.js
@@ -22,75 +22,54 @@ const fileNames = [
     'items/sample.json'
 ];
 
-// Function to fetch and draw scatter plot from a local JSON file
-function drawScatterPlot(filename) {
-    // Extract the chart name from the filename (remove ".json" at the end and "items/")
-    const chartName = filename.replace('items/', '').replace('.json', '');
-
-    fetch(filename)
-        .then(response => response.json())
-        .then(data => {
-            const dates = data.map(item => item.Date);
-            const prices = data.map(item => parseFloat(item.Price));
-            const discounts = data.map(item => parseFloat(item.Discount || 0)); // Default to 0 if Discount is not present
-
-            // Format dates to display only Day, Month, and Year
-            const formattedDates = dates.map(date => new Date(date).toISOString().slice(0, 10));
-
-            const trace = {
-                x: formattedDates,
-                y: prices,
-                mode: 'markers',
-                type: 'scatter',
-                marker: { 
-                    size: 8,
-                    color: discounts.map(discount => discount > 0 ? 'red' : 'green') // Color points based on discount
-                },
-                line: {
-                    color: discounts.map(discount => discount > 0 ? 'lightred' : 'lightgreen'), // Color lines based on discount
-                    width: 1 // Line width
-                }
-            };
-
-            const layout = {
-                title: '', // Empty title (no need for a title as <h3> contains the chart name)
-                xaxis: { title: 'Date' },
-                yaxis: { title: 'Price' }
-            };
-
-            const plotData = [trace];
-
-            // Create a new div for each plot
-            const div = document.createElement('div');
-            div.className = 'scatter-plot';
-            document.getElementById('scatter-plots').appendChild(div);
-
-            // Create an <h3> tag for the chart name and set its text content
-            const chartTitle = document.createElement('h3');
-            chartTitle.textContent = chartName; // Set the chart name
-            chartTitle.style.color = 'darkblue'; // Apply dark blue color
-
-            // Create a container <div> for <h3> and the scatter plot
-            const chartContainer = document.createElement('div');
-            chartContainer.className = 'chart-container';
-
-            // Append the <h3> and the scatter plot to the container <div>
-            chartContainer.appendChild(chartTitle);
-            chartContainer.appendChild(div);
-
-            // Append the container <div> to the scatter-plots container
-            document.getElementById('scatter-plots').appendChild(chartContainer);
-
-            // Render the scatter chart in the new div
-            Plotly.newPlot(div, plotData, layout);
-        })
-        .catch(error => console.error('Error loading data:', error));
-}
-
 document.addEventListener('DOMContentLoaded', function () {
-    // Loop through the file names and draw scatter plots
-    for (const fileName of fileNames) {
-        drawScatterPlot(fileName);
-}
-});
+    for (let i = 0; i < fileNames.length; i++) {
+        const fileName = fileNames[i];
+        const chartName = fileName.replace('items/', '').replace('.json', '');
 
+        fetch(fileName)
+            .then(response => response.json())
+            .then(data => {
+                const dates = data.map(item => item.Date);
+                const prices = data.map(item => parseFloat(item.Price));
+                const discounts = data.map(item => parseFloat(item.Discount || 0));
+
+                const formattedDates = dates.map(date => new Date(date).toISOString().slice(0, 10));
+
+                const trace = {
+                    x: formattedDates,
+                    y: prices,
+                    mode: 'line+markers',
+                    type: 'scatter',
+                    marker: {
+                        size: 8,
+                        color: discounts.map(discount => discount > 0 ? 'red' : 'green')
+                    },
+                    line: {
+                        color: discounts.map(discount => discount > 0 ? 'lightred' : 'lightgreen'),
+                        width: 1
+                    }
+                };
+
+                const layout = {
+                    xaxis: { title: 'Date' },
+                    yaxis: { title: 'Price' }
+                };
+
+                const plotData = [trace];
+
+                const div = document.createElement('div');
+                div.className = 'scatter-plot';
+                document.getElementById('scatter-plots').appendChild(div);
+
+                const chartTitle = document.createElement('h3');
+                chartTitle.textContent = chartName;
+                chartTitle.style.color = 'darkblue';
+                chartTitle.id = `chart-${i + 1}`;
+                div.appendChild(chartTitle);
+
+                Plotly.newPlot(div, plotData, layout);
+            })
+            .catch(error => console.error('Error loading data:', error));
+    }
+});
