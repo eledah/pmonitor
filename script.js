a675822199fb5767de801a6c8b23a6006e2d133c
Eledah <eledah.niles@yahoo.com>
2023-09-20 13:46:23 +0330

    Callout fix

--- script.js
+++ script.js
@@ -37,36 +37,40 @@ document.addEventListener('DOMContentLoaded', function () {
                 const formattedDates = dates.map(date => new Date(date).toISOString().slice(0, 10));
 
                 const trace = {
+                    name: '',
                     x: formattedDates,
                     y: prices,
                     mode: 'lines+markers',
                     type: 'scatter',
                     marker: {
                         size: 8,
-                        color: discounts.map(discount => discount > 0 ? 'rgb(198, 40, 40)' : 'rgb(46, 125, 50)')
+                        color: discounts.map(discount => discount > 0 ? 'rgb(198, 40, 40)' : 'rgb(46, 125, 50)'),
                     },
                     line: {
-                        color: 'rgb(165, 214, 167)',
+                        color: 'rgb(200, 200, 200)',
                         width: 2
-                    }
+                    },
+                    hovertemplate: '<b>%{y}</b><br>%{text}% OFF',
+                    text: discounts
                 };
 
                 const layout = {
-                    // autosize: false,
-                    // width: 1000,
-                    // height: 500,
                     xaxis: { title: 'تاریخ' },
-                    yaxis: { title: 'قیمت', },
+                    yaxis: { title: 'قیمت' },
                     font: {
-                        family: 'Sahel FD',
+                        family: 'Roboto',
                         size: 12,
                         color: '#7f7f7f'
+                    },
+                    hoverlabel: {
+                        font: {
+                          family: 'Sahel FD', // Specify the desired font family
+                          size: 12, // Specify the font size
+                        }
                     }
-
                 };
 
                 const plotData = [trace];
-
 
                 const mainContainer = document.getElementById('scatter-plots')
                 const containerDiv = document.createElement('div');
@@ -84,7 +88,6 @@ document.addEventListener('DOMContentLoaded', function () {
                 containerDiv.appendChild(chartTitle)
                 containerDiv.appendChild(scatterDiv)
                 
-                // mainContainer.appendChild(document.createElement('br'));
                 mainContainer.appendChild(containerDiv);
 
                 Plotly.newPlot(scatterDiv, plotData, layout);
