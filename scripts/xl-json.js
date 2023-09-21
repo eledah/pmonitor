b58f86de850b59b99c6a868b131c5ba89e7a5a02
Eledah <eledah.niles@yahoo.com>
2023-09-21 14:53:59 +0330

    json-input

--- scripts/xl-json.js
+++ scripts/xl-json.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const xlsx = require('xlsx');
 
-const INPUT_FOLDER = '../items'; // Change this to the folder containing Excel files
+const INPUT_FOLDERS = ['../items', '../']; // Add the paths of the folders you want to scan and change
 
 function convertExcelToJson(filePath) {
   const workbook = xlsx.readFile(filePath);
@@ -14,18 +14,21 @@ function convertExcelToJson(filePath) {
   return jsonData;
 }
 
-// Read and convert all Excel files in the input folder
-fs.readdirSync(INPUT_FOLDER).forEach((file) => {
-  if (file.endsWith('.xlsx')) {
-    const excelFilePath = path.join(INPUT_FOLDER, file);
-    const jsonData = convertExcelToJson(excelFilePath);
+// Iterate through each input folder
+for (const inputFolder of INPUT_FOLDERS) {
+  // Read and convert all Excel files in the current input folder
+  fs.readdirSync(inputFolder).forEach((file) => {
+    if (file.endsWith('.xlsx')) {
+      const excelFilePath = path.join(inputFolder, file);
+      const jsonData = convertExcelToJson(excelFilePath);
 
-    // Construct the output JSON file path with the same name as the input file
-    const jsonFileName = path.join(INPUT_FOLDER, path.basename(file, '.xlsx') + '.json');
-    
-    fs.writeFileSync(jsonFileName, JSON.stringify(jsonData, null, 2));
-    console.log(`Converted ${excelFilePath} to ${jsonFileName}`);
-  }
-});
+      // Construct the output JSON file path with the same name as the input file
+      const jsonFileName = path.join(inputFolder, path.basename(file, '.xlsx') + '.json');
+      
+      fs.writeFileSync(jsonFileName, JSON.stringify(jsonData, null, 2));
+      console.log(`Converted ${excelFilePath} to ${jsonFileName}`);
+    }
+  });
+}
 
 console.log('Conversion completed!');
