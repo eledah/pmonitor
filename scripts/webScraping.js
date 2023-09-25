2302cdf9ac4df24292a7ae7fb70d9d7edb73067a
Eledah <eledah.niles@yahoo.com>
2023-09-25 12:25:21 +0330

    Price bugfix

--- scripts/webScraping.js
+++ scripts/webScraping.js
@@ -6,9 +6,104 @@ const path = require('path');
 // Set this constant to true to enable verbose logging
 const VERBOSE = true;
 
+// Set OUTPUT to true to alter .xlsx files, or false to skip altering
+const OUTPUT = true;
+
 const INPUT_FILE_PATH = '../items.xlsx';
 const OUTPUT_DIRECTORY = '../items';
 const OUTPUT_FILE_PATH = '../output.xlsx';
+
+// Declare currentDate at the beginning of the script
+const currentDate = new Date().toISOString().split('T')[0];
+
+async function getPrice(page) {
+  const discountedSelector = '.line-through.text-body-2.ml-1.text-neutral-300'
+  const discounted = await page.$(discountedSelector);
+
+  let masterDivSelector = ''
+  let masterDiv = ''
+
+  // If an item is discounted, then its nesting price <div> differs for some reason.
+  if (discounted) {
+    VERBOSE ? console.log('Item is discounted') : null;
+
+    // Use page.$() to select the <div> element
+    masterDiv = await page.$('div.items-end:nth-child(2) > div:nth-child(2)');
+
+  } else {
+    VERBOSE ? console.log('Item is not discounted') : null;
+
+    masterDivSelector = '.flex.items-center.justify-end.w-full'
+    masterDiv = await page.$(masterDivSelector)
+  }
+
+  console.log(masterDiv)
+
+  const priceDivSelector = 'span.text-neutral-800.ml-1.text-h4'
+  const priceElement = await masterDiv.$(priceDivSelector);
+
+
+  return priceElement
+    ? (await (await priceElement.getProperty('textContent')).jsonValue())
+        .replace(/,/g, '') // Remove commas
+        .replace(/[۱۲۳۴۵۶۷۸۹۰]/g, (match) => String.fromCharCode(match.charCodeAt(0) - 1728)) // Convert Farsi numbers to English
+    : 'Price not found';
+}
+
+async function getDiscount(page) {
+  const masterDivSelector = '.flex.items-center.justify-end.w-full'
+  const discountDivSelector = '.px-1.text-white.rounded-large.flex.items-center.justify-center.ProductPrice_ProductPrice__discountWrapper__1Ru_1.bg-primary-700.shrink-0.mr-1';
+
+  const masterDiv = await page.$(masterDivSelector)
+  const discountContainer = await masterDiv.$(discountDivSelector);
+
+  let discount = "0";
+
+  if (discountContainer) {
+    // Use divHandle.$ to select the nested span
+    const nestedSpanSelector = 'span.text-body2-strong';
+    const discountHandle = await discountContainer.$(nestedSpanSelector);
+
+    if (discountHandle) {
+      // Get the innerHTML of the selected span
+      discount = await (await page.evaluate(element => element.innerHTML, discountHandle))
+        .replace("٪", "")
+        .replace(/[۱۲۳۴۵۶۷۸۹۰]/g, (match) => String.fromCharCode(match.charCodeAt(0) - 1728))
+      console.log("Discount Percentage: ", discount);
+    } else {
+      console.log("No span with class 'text-body2-strong' found inside the first div.");
+      discount = "0"
+    }
+  } else {
+    console.log("No div with the specified class found on the page.");
+    discount = "0"
+  }
+
+  return discount;
+}
+
+async function writeToExcel(itemName, price, discount) {
+  // Create an Excel file for each item and append data
+  const itemFileName = `${itemName}.xlsx`;
+  const itemFilePath = path.join(OUTPUT_DIRECTORY, itemFileName);
+
+  const itemWorkbook = new ExcelJS.Workbook();
+  if (fs.existsSync(itemFilePath)) {
+    await itemWorkbook.xlsx.readFile(itemFilePath);
+  }
+
+  let itemWorksheet = itemWorkbook.getWorksheet(1);
+  if (!itemWorksheet) {
+    itemWorksheet = itemWorkbook.addWorksheet('Sheet 1');
+    itemWorksheet.addRow(['Date', 'Price', 'Discount']); // Header row
+  }
+
+  itemWorksheet.addRow([currentDate, price, discount]);
+
+  await itemWorkbook.xlsx.writeFile(itemFilePath);
+
+  console.log(`Data appended to ${itemFileName}`);
+}
 
 async function scrapeWebpages() {
   const browser = await puppeteer.launch();
@@ -31,7 +126,7 @@ async function scrapeWebpages() {
     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
     // Wait for 10 seconds (adjust as needed)
-    await new Promise((resolve) => setTimeout(resolve, 10000)); // 10 seconds
+    await new Promise((resolve) => setTimeout(resolve, 10000)); // 15 seconds
 
     // Check if the page is loaded
     const isPageLoaded = await page.evaluate(() => {
@@ -43,64 +138,14 @@ async function scrapeWebpages() {
         console.log(`Page loaded successfully for item: ${itemName}`);
       }
 
-      const priceElement = await page.$('span.text-neutral-800.ml-1.text-h4');
-      const price = priceElement
-        ? (await (await priceElement.getProperty('textContent')).jsonValue())
-            .replace(/,/g, '') // Remove commas
-            .replace(/[۱۲۳۴۵۶۷۸۹۰]/g, (match) => String.fromCharCode(match.charCodeAt(0) - 1728)) // Convert Farsi numbers to English
-        : 'Price not found';
-
+      const price = await getPrice(page);
       console.log("Price: ", price);
 
-      const masterDivSelector = '.flex.items-center.justify-end.w-full'
-      const discountDivSelector = '.px-1.text-white.rounded-large.flex.items-center.justify-center.ProductPrice_ProductPrice__discountWrapper__1Ru_1.bg-primary-700.shrink-0.mr-1';
-                        
-      const masterDiv = await page.$(masterDivSelector)
-      const discountContainer = await masterDiv.$(discountDivSelector);
+      const discount = await getDiscount(page);
 
-      let discount = "0";
-
-      if (discountContainer) {
-        // Use divHandle.$ to select the nested span
-        const nestedSpanSelector = 'span.text-body2-strong';
-        const discountHandle = await discountContainer.$(nestedSpanSelector);
-
-        if (discountHandle) {
-          // Get the innerHTML of the selected span
-          discount = await (await page.evaluate(element => element.innerHTML, discountHandle))
-            .replace("٪", "")
-            .replace(/[۱۲۳۴۵۶۷۸۹۰]/g, (match) => String.fromCharCode(match.charCodeAt(0) - 1728))
-          console.log("Discount Percentage: ", discount);
-        } else {
-          console.log("No span with class 'text-body2-strong' found inside the first div.");
-          discount = "0"
-        }
-      } else {
-        console.log("No div with the specified class found on the page.");
-        discount = "0"
+      if (OUTPUT) {
+        await writeToExcel(itemName, price, discount);
       }
-
-      // Create an Excel file for each item and append data
-      const itemFileName = `${itemName}.xlsx`;
-      const itemFilePath = path.join(OUTPUT_DIRECTORY, itemFileName);
-
-      const itemWorkbook = new ExcelJS.Workbook();
-      if (fs.existsSync(itemFilePath)) {
-        await itemWorkbook.xlsx.readFile(itemFilePath);
-      }
-
-      let itemWorksheet = itemWorkbook.getWorksheet(1);
-      if (!itemWorksheet) {
-        itemWorksheet = itemWorkbook.addWorksheet('Sheet 1');
-        itemWorksheet.addRow(['Date', 'Price', 'Discount']); // Header row
-      }
-
-      const currentDate = new Date().toISOString().split('T')[0];
-      itemWorksheet.addRow([currentDate, price, discount]);
-
-      await itemWorkbook.xlsx.writeFile(itemFilePath);
-
-      console.log(`Data appended to ${itemFileName}`);
       
       // Add the program's output to the outputWorkbook
       let outputSheet = outputWorkbook.getWorksheet(currentDate);
@@ -117,8 +162,11 @@ async function scrapeWebpages() {
     }
   }
 
-  // Save the outputWorkbook to output.xlsx in the main directory
-  await outputWorkbook.xlsx.writeFile(OUTPUT_FILE_PATH);
+  if (OUTPUT) {
+    // Save the outputWorkbook to output.xlsx in the main directory
+    await outputWorkbook.xlsx.writeFile(OUTPUT_FILE_PATH);
+  }
+  
   await browser.close();
 }
 
